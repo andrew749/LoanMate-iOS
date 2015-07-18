8db736cae69549622b4a46d34007655f237ce61f
Andrew Codispoti <andrewcod749@gmail.com>
2015-07-18 10:20:04 -0400

    Initial Commit

---
+++ LoanMate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoanMate
+//
+//  Created by Andrew Codispoti on 2015-07-18.
+//  Copyright (c) 2015 Andrew Codispoti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
